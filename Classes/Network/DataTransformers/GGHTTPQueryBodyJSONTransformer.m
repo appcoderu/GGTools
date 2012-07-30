0302774cc9206e9058375e7ae17fc5f14f2b5947
Evgeniy Shurakov <e@shurakov.name>
2012-07-30 14:36:25 +0600

    Use SBJson if NSJson is not available

--- Classes/Network/DataTransformers/GGHTTPQueryBodyJSONTransformer.m
+++ Classes/Network/DataTransformers/GGHTTPQueryBodyJSONTransformer.m
@@ -13,20 +13,38 @@
 
 #import "NSError+GGExtra.h"
 
+#import "SBJsonParser.h"
+#import "SBJsonWriter.h"
+
 @implementation GGHTTPQueryBodyJSONTransformer
 
 + (id)decode:(NSData *)data error:(NSError **)error {
 	NSError *jsonError = nil;
-	id object = [NSJSONSerialization JSONObjectWithData:data
-												options:0
-												  error:&jsonError];
+	id object = nil;
 	
-	if (jsonError && error) {
-		*error = [NSError gg_errorWithDomain:kGGHTTPServiceErrorDomain
-										code:kGGHTTPServiceErrorInvalidResponseData
-								 description:NSLocalizedString(@"Error", nil)
-							   failureReason:jsonError.localizedFailureReason
-							 underlyingError:jsonError];
+	Class jsonSerializaionClass = nil;
+	if ((jsonSerializaionClass = NSClassFromString(@"NSJSONSerialization"))) {
+		object = [jsonSerializaionClass JSONObjectWithData:data
+												   options:0
+													 error:&jsonError];
+		if (jsonError && error) {
+			*error = [NSError gg_errorWithDomain:kGGHTTPServiceErrorDomain
+											code:kGGHTTPServiceErrorInvalidResponseData
+									 description:NSLocalizedString(@"Error", nil)
+								   failureReason:jsonError.localizedFailureReason
+								 underlyingError:jsonError];
+		}
+	} else if ((jsonSerializaionClass = NSClassFromString(@"SBJsonParser"))) {
+		id jsonSerializaionObj = [[jsonSerializaionClass alloc] init];
+		object = [jsonSerializaionObj objectWithData:data];
+		if (!object && error) {
+			*error = [NSError gg_errorWithDomain:kGGHTTPServiceErrorDomain
+											code:kGGHTTPServiceErrorInvalidResponseData
+									 description:NSLocalizedString(@"Error", nil)
+								   failureReason:(NSString *)[jsonSerializaionObj error]];
+		}
+	} else {
+		NSAssert(false, @"JSON parser not found (NSJSONSerialization or SBJsonParser)");
 	}
 	
 	return object;
@@ -37,22 +55,41 @@
 		return nil;
 	}
 	
-	NSError *jsonError = nil;
-	NSData *data = [NSJSONSerialization dataWithJSONObject:bodyObject
-												   options:0
-													 error:&jsonError];
+	NSData *data = nil;
 	
-	if (jsonError) {
-		if (error) {
-			*error = [NSError gg_errorWithDomain:kGGHTTPServiceErrorDomain
-											code:kGGHTTPServiceErrorInvalidRequestBody
-									 description:NSLocalizedString(@"Error", nil)
-								   failureReason:jsonError.localizedFailureReason
-								 underlyingError:jsonError];
+	Class jsonSerializaionClass = nil;
+	if ((jsonSerializaionClass = NSClassFromString(@"NSJSONSerialization"))) {
+		NSError *jsonError = nil;
+		data = [jsonSerializaionClass dataWithJSONObject:bodyObject
+												 options:0
+												   error:&jsonError];
+		if (jsonError) {
+			if (error) {
+				*error = [NSError gg_errorWithDomain:kGGHTTPServiceErrorDomain
+												code:kGGHTTPServiceErrorInvalidRequestBody
+										 description:NSLocalizedString(@"Error", nil)
+									   failureReason:jsonError.localizedFailureReason
+									 underlyingError:jsonError];
+			}
+			return nil;
 		}
+	} else if ((jsonSerializaionClass = NSClassFromString(@"SBJsonWriter"))) {
+		id jsonSerializaionObj = [[jsonSerializaionClass alloc] init];
+		data = [jsonSerializaionObj dataWithObject:bodyObject];
+		if (!data || [data length] == 0) {
+			if (error) {
+				*error = [NSError gg_errorWithDomain:kGGHTTPServiceErrorDomain
+												code:kGGHTTPServiceErrorInvalidRequestBody
+										 description:NSLocalizedString(@"Error", nil)
+									   failureReason:(NSString *)[jsonSerializaionObj error]];
+			}
+			return nil;
+		}
+	} else {
+		NSAssert(false, @"JSON writer not found (NSJSONSerialization or SBJsonWriter)");
 		return nil;
 	}
-	
+
 	GGHTTPQueryBody *body = [[GGHTTPQueryBody alloc] init];
 	body.data = data;
 	body.contentType = @"application/json";
